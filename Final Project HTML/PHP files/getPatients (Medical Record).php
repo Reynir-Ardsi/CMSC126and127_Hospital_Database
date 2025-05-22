33fd2bed1bd50112ec34f875b3cfcb5c62fafde1
CSTwist <125715223+CSTwist@users.noreply.github.com>
2025-05-22 23:17:32 +0800

    for debugging

--- Final Project HTML/PHP files/getPatients (Medical Record).php
+++ Final Project HTML/PHP files/getPatients (Medical Record).php
@@ -1,8 +1,7 @@
 <?php
 include 'DBConnector.php';
 
-$sql = "SELECT *
-        FROM patient";
+$sql = "SELECT * FROM patient";
 
 $result = $conn->query($sql);
 
@@ -12,6 +11,7 @@ if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $patients[] = $row;
     }
+    var_dump($patients);
 }
 
 header('Content-Type: application/json');
