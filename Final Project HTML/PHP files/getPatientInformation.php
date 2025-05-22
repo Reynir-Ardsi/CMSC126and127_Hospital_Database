149632c0cfe80ff5f96eea1a7fed8924ca11f999
CSTwist <125715223+CSTwist@users.noreply.github.com>
2025-05-22 23:59:04 +0800

    try

--- Final Project HTML/PHP files/getPatientInformation.php
+++ Final Project HTML/PHP files/getPatientInformation.php
@@ -1,6 +1,9 @@
 <?php
 include 'DBConnector.php';
 
+// Get the patient ID from the URL query parameter
+$patientId = $_GET['id'];  // This retrieves the 'id' from the URL
+
 // SQL query to fetch details of the patient
 $sql = "SELECT * FROM patients WHERE id = ?";
 $stmt = $conn->prepare($sql);
