df89cdd6e1d2d5289020fc1f744fc3340ce9c7f0
Chakinzo Sombito <schakinzo@gmail.com>
2025-05-23 09:15:45 +0800

    getAllAppointments.php

---
+++ Final Project HTML/PHP files/getAllAppointments.php
@@ -0,0 +1,34 @@
+<?php
+include 'DBConnector.php';
+
+$sql = "
+    SELECT DISTINCT 
+        appointment.appointment_id,
+        users.first_name AS doctor_first_name,
+        users.last_name AS doctor_last_name,
+        patient.first_name AS patient_first_name,
+        patient.middle_initial,
+        patient.last_name AS patient_last_name,
+        appointment.time,
+        appointment.room_number,
+        appointment.status,
+        appointment.appointment_date
+    FROM appointment
+    INNER JOIN patient ON appointment.patient_id = patient.patient_id
+    INNER JOIN users ON appointment.doctor_id = users.user_id
+";
+
+$result = $conn->query($sql);
+
+$appointments = [];
+
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $appointments[] = $row;
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode($appointments);
+$conn->close();
+?>
