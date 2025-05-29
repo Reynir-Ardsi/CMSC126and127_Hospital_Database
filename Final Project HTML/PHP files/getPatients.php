819477891b6dabc744a1d652868bc66a9d953349
Chakinzo Sombito <schakinzo@gmail.com>
2025-05-29 20:07:08 +0800

    Billing updates

--- Final Project HTML/PHP files/getPatients.php
+++ Final Project HTML/PHP files/getPatients.php
@@ -4,16 +4,29 @@ include 'DBConnector.php';
 
 // Fetch all patients
 $sql = "
-  SELECT *
-    FROM patient
+  SELECT 
+  appointment.*, 
+  patient.patient_id,
+  patient.first_name AS patient_first,
+  patient.middle_initial AS patient_middle,
+  patient.last_name AS patient_last,
+  users.user_id AS doctor_id,
+  users.first_name AS doctor_first, 
+  users.middle_initial AS doctor_middle, 
+  users.last_name AS doctor_last
+FROM patient
+INNER JOIN appointment ON appointment.patient_id = patient.patient_id
+INNER JOIN users ON users.user_id = appointment.doctor_id
 ";
 $result = $conn->query($sql);
 
 $patients = [];
 while ($row = $result->fetch_assoc()) {
     $patients[] = [
-        'id'   => $row['patient_id'],
-        'name' => trim($row['first_name'] . ' ' . $row['middle_initial'] . ' ' . $row['last_name'])
+        'id'          => $row['patient_id'],
+        'name'        => trim($row['patient_first'] . ' ' . $row['patient_middle'] . ' ' . $row['patient_last']),
+        'doctor_name' => trim($row['doctor_first'] . ' ' . $row['doctor_middle'] . ' ' . $row['doctor_last']),
+        'doctor_id'   => $row['doctor_id']
     ];
 }
 
