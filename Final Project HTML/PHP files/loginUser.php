a753ae0624688421aa61107d03c6fa7adee2f429
HewRMyFire <fmatthewsimpas@gmail.com>
2025-05-30 00:19:59 +0800

    hashed password for login

--- Final Project HTML/PHP files/loginUser.php
+++ Final Project HTML/PHP files/loginUser.php
@@ -3,50 +3,42 @@ include 'DBConnector.php';
 
 $userName = $_POST["userName"];
 $password = $_POST["password"];
-$validUsername = false;
-$validPassword = false;
-$user_id = null;
 $response = [];
 
-$authenticateUsername = "SELECT user_id FROM login WHERE username = ?";
-$stmt = $conn->prepare($authenticateUsername);
+$authenticate = "SELECT user_id, password FROM login WHERE username = ?";
+$stmt = $conn->prepare($authenticate);
 $stmt->bind_param("s", $userName);
 $stmt->execute();
-$userNameResult = $stmt->get_result();
+$result = $stmt->get_result();
 
-if ($userNameResult->num_rows > 0) {
-    $validUsername = true;
-    $row = $userNameResult->fetch_assoc();
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
     $user_id = $row['user_id'];
+    $hashedPassword = $row['password'];
 
-    $authenticatePassword = "SELECT user_id FROM login WHERE user_id = ? AND password = ?";
-    $stmt = $conn->prepare($authenticatePassword);
-    $stmt->bind_param("is", $user_id, $password);
-    $stmt->execute();
-    $passwordResult = $stmt->get_result();
+    if (password_verify($password, $hashedPassword)) {
+        $findJob = "SELECT role FROM job WHERE user_id = ?";
+        $stmt = $conn->prepare($findJob);
+        $stmt->bind_param("s", $user_id);
+        $stmt->execute();
+        $job = $stmt->get_result();
+        $jobRow = $job->fetch_assoc();
 
-    if ($passwordResult->num_rows > 0) {
-        $validPassword = true;
+        $response = [
+            "status" => "success",
+            "user_id" => $user_id,
+            "role" => $jobRow['role']
+        ];
+    } else {
+        $response = [
+            "status" => "error",
+            "message" => "Invalid Password"
+        ];
     }
-}
-
-if ($validUsername && $validPassword) {
-    $findJob = "SELECT role FROM job WHERE user_id = ?";
-    $stmt = $conn->prepare($findJob);
-    $stmt->bind_param("i", $user_id);
-    $stmt->execute();
-    $job = $stmt->get_result();
-    $jobRow = $job->fetch_assoc();
-
-    $response = [
-        "status" => "success",
-        "user_id" => $user_id,
-        "role" => $jobRow['role']
-    ];
 } else {
     $response = [
         "status" => "error",
-        "message" => !$validUsername ? (!$validPassword ? "Invalid Username and Password" : "Invalid Username") : "Invalid Password"
+        "message" => "Invalid Username"
     ];
 }
 
